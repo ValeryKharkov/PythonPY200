d946872bb8c1772c398108ba13ba22a9a161c17c
ValeryKharkov <kharkov.valery@gmail.com>
2022-06-09 00:05:08 +0300

    В процессе выполнения домашнего упражнения: task9_Date

--- Основы ООП/Домашнее задание/task9_Date/task.py
+++ Основы ООП/Домашнее задание/task9_Date/task.py
@@ -1,1 +1,28 @@
+class Date:
+    def __init__(self, day: int, month: int, year: int):
+        """
+        Создание класса Date и проверка на передачу целочисленных значений
+        :param day: день
+        :param month: месяц
+        :param year: год
+        """
+        self.day = day
+        self.month = month
+        self.year = year
+
+        if not isinstance(day or month or year, int):
+            raise TypeError
+
+    def __repr__(self):
+        return f"Результат работы метода __repr__: {self.day}/{self.month}/{self.year}"
+
+    def __str__(self):
+        return f"Результат работы метода __str__: {self.day}/{self.month}/{self.year}"
+
+
+result_1 = Date(2, '45', 78)
+
+
+print(result_1)
+
 # TODO class Date
