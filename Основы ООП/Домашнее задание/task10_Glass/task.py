fc187db78e535b4cf9289187b27ebf3142810f84
ValeryKharkov <kharkov.valery@gmail.com>
2022-06-08 22:43:20 +0300

    Выполнено домашнее упражнение: task10_Glass

--- Основы ООП/Домашнее задание/task10_Glass/task.py
+++ Основы ООП/Домашнее задание/task10_Glass/task.py
@@ -1,6 +1,17 @@
-# TODO написать класс Glass согласно условию
+# написать класс Glass согласно условию
+class Glass:
+    def __init__(self, material: str):
+        """
+        Создание класса с одним атрибутом и одним методом
+        :param material: материал объекта
+        """
+        self.material_attr = material
+
+    def get_material(self):
+        return self.material_attr
 
 
 if __name__ == "__main__":
-    ...
-
+    result = Glass('paper')
+    print(result)
+# TODO что сделать, что бы консоль печатала "paper"
